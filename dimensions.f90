b2626efafe03818327c3a4ecc5fa4f78950ab716
atvu1220 <atvu1220@gmail.com>
2023-05-23 19:30:35 -0400

    first commit, branch from local-hybrid-TD

--- dimensions.f90
+++ dimensions.f90
@@ -1,7 +1,8 @@
 module dimensions
       implicit none
       save
-      integer, parameter:: nx = 902, ny = 3, nz = 600 
+      !integer, parameter:: nx = 902, ny = 3, nz = 600 
+      integer, parameter:: nx = 302, ny = 3, nz = 200 
       integer, parameter:: Ni_max =6000000
            
 end module dimensions
